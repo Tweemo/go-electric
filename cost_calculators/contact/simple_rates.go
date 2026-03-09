f2239f697de314e1a1c29ed8ea12ebf27c90a9b6
Tweemo <timothyliew88@hotmail.com>
2026-03-09 17:28:02 +1300

    Matching Contact Simple_rates to Nova general rates

--- cost_calculators/contact/simple_rates.go
+++ cost_calculators/contact/simple_rates.go
@@ -6,22 +6,21 @@ import (
 
 func SimpleRatesStandardUser(sortedRecords []utils.DayPower) float64 {
 	rate := utils.GetRate("Contact", "GoodNights", "standard")
-	usage := utils.TotalUsage(sortedRecords)
 
-	totalCost := CalculateSimpleRatesCost(usage, rate)
+	totalCost := CalculateSimpleRatesCost(sortedRecords, rate)
 	return totalCost
 }
 
 func SimpleRatesLowUser(sortedRecords []utils.DayPower) float64 {
 	rate := utils.GetRate("Contact", "GoodNights", "low")
-	usage := utils.TotalUsage(sortedRecords)
 
-	totalCost := CalculateSimpleRatesCost(usage, rate)
+	totalCost := CalculateSimpleRatesCost(sortedRecords, rate)
 	return totalCost
 }
 
-func CalculateSimpleRatesCost(usage float64, rate utils.Rate) float64 {
+func CalculateSimpleRatesCost(sortedRecords []utils.DayPower, rate utils.Rate) float64 {
 	levy := utils.GetLevy("Contact")
+	usage := utils.TotalUsage(sortedRecords)
 
 	cost := (rate.Pwh + levy) * usage
 	cost += rate.Daily * 30
